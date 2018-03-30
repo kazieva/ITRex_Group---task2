08ebd7f84f847ca36eb6e9fefb8345e4ba9b205d
kazieva <32552023+kazieva@users.noreply.github.com>
2018-03-30 18:30:22 +0200

    Labyrinth, log4j

---
+++ src/by/bsu/mmf/kazieva/Main.java
@@ -0,0 +1,15 @@
+package by.bsu.mmf.kazieva;
+
+import by.bsu.mmf.kazieva.labyrinth.Labyrinth;
+
+/**
+ * Created by YiaKrevetko on 28.03.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Labyrinth labyrin = new Labyrinth();
+        labyrin.algoritm();
+
+    }
+
+}
